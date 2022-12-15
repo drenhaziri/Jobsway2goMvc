234f512b2ab77c05f9ecaedf5ed2edccc0b4a43a
MedinaRexhepi <102358051+MedinaRexhepi@users.noreply.github.com>
2022-12-15 10:25:52 +0100

    Added JobCategory model

--- Jobsway2goMvc/Models/JobCategory.cs
+++ Jobsway2goMvc/Models/JobCategory.cs
@@ -4,7 +4,6 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public int JobId { get; set; }
-      //  public Job Job { get; set; }
+        //public List<Job> Jobs {get; set;}
     }
 }
