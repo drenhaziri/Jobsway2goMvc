7aa3c3ab942f54921dc3b8346067d8237ba85350
Rron <rrqirezi@gmail.com>
2022-12-15 10:14:17 +0100

    J3- Group model Added properties for group

---
+++ Jobsway2goMvc/Models/Group.cs
@@ -0,0 +1,10 @@
+﻿namespace Jobsway2goMvc.Models
+{
+    public class Group
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        //public List<AdminUser> Admin {get;set;}
+        //public List<ModeratoUser> Moderator {get;set;}
+    }
+}
