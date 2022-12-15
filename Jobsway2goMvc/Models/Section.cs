360304d6855289b2365a2e33956986830d4d3cad
Agnesa <agnesa.28@hotmail.com>
2022-12-15 11:04:16 +0100

    Added Section model properties

---
+++ Jobsway2goMvc/Models/Section.cs
@@ -0,0 +1,10 @@
+﻿namespace Jobsway2goMvc.Models;
+
+public class Section
+{
+    public  int Id { get; set; }
+    public string Name { get; set; }
+    public string Location { get; set; }
+    public DateTime DateFrom { get; set; }
+    public DateTime DateTo { get; set; }
+}
