bc7f2e2bfbcde1f7bc1784d1b3034b72e309373b
l1roii <71549088+l1roii@users.noreply.github.com>
2022-12-15 10:02:19 +0100

    Created Collection model

---
+++ Jobsway2goMvc/Models/Collection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jobsway2goMvc.Models
+{
+    public class Collection
+    {
+        public int Id { get; set; }
+        //public ApplicationUser User { get; set; }
+        public string Name { get; set; }
+        //public List<Job> Job { get; set; }
+        //public List<Post> Post { get; set; }
+    }
+}
+
