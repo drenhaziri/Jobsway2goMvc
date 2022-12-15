3f09d531107635acf0254bec3985684d4f1e6208
medinahaliti <medina.haliti11@gmail.com>
2022-12-15 20:39:14 +0100

    Added properties on Event model

--- Jobsway2goMvc/Models/Event.cs
+++ Jobsway2goMvc/Models/Event.cs
@@ -2,5 +2,13 @@
 {
     public class Event
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string CompanyName { get; set; }
+        public string ImagePath { get; set; }
+        public List<ApplicationUser> Speakers { get; set; }
+        public string URL { get; set; }
+        public string Location { get; set; }
     }
 }
