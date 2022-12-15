102108545e1896b54c7492ccd26ef521456c6a5f
Diell02 <diellkepuska@gmail.com>
2022-12-15 10:20:08 +0100

    PostModel, PostType

---
+++ Jobsway2goMvc/Models/Post.cs
@@ -0,0 +1,14 @@
+﻿using Jobsway2goMvc.Enums;
+
+namespace Jobsway2goMvc.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedAtUTC { get; set; }
+        public string CreatedByUserId { get; set; }
+        public PostType Type { get; set; }
+    }
+}
