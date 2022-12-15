526e4957904cf3b6b75dee826a03d7e1c0c412f9
Rron <rrqirezi@gmail.com>
2022-12-15 10:25:15 +0100

    J3-Group Model Added the properties and lists

--- Jobsway2goMvc/Models/Group.cs
+++ Jobsway2goMvc/Models/Group.cs
@@ -4,6 +4,7 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public List<ApplicationUser> Users { get; set; }
         //public List<AdminUser> Admin {get;set;}
         //public List<ModeratoUser> Moderator {get;set;}
     }
