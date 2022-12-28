b7f9e20b24b660378daecd874614a38b0c8562f7
l1roii <71549088+l1roii@users.noreply.github.com>
2022-12-28 17:31:08 +0100

    Added CollectionJobViewModel and it's properties

---
+++ Jobsway2goMvc/Models/ViewModel/CollectionJobViewModelcs.cs
@@ -0,0 +1,8 @@
+﻿namespace Jobsway2goMvc.Models.ViewModel
+{
+    public class CollectionJobViewModelcs
+    {
+        public Job Jobs { get; set; }
+        public Collection Collections { get; set; }
+    }
+}
