6841480f1276d35027f5c34525a760690a7bf86c
medinaarexhepii <medinaa.rexhepi1@gmail.com>
2023-01-04 11:24:10 -0800

    Fixed broken user role assign when registering

--- Jobsway2goMvc/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Jobsway2goMvc/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -128,9 +128,15 @@ namespace Jobsway2goMvc.Areas.Identity.Pages.Account
                 user.LastName = Input.LastName;
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
-                var result = await _userManager.CreateAsync(user, Input.Password);
+               
 
                 var userRole = _roleManager.FindByNameAsync("User").Result;
+                if (userRole == null)
+                {
+                    ViewData["UserRoleIsNull"] = "Something wrong happened";
+                    return Page();
+                }
+                var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
