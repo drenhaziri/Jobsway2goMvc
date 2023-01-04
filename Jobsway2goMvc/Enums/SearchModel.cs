5696f19c46449effe37b5fda1fd590e90257e49e
l1roii <71549088+l1roii@users.noreply.github.com>
2023-01-04 17:11:58 +0100

    Created SearchEnum Enum

---
+++ Jobsway2goMvc/Enums/SearchModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Jobsway2goMvc.Enums
+{
+    public enum SearchModel
+    {
+        [Display(Name = "Select Type")]
+        SelectType,
+        User,
+        Job,
+        Event,
+        Group
+    }
+}
