ff66b6f7ed96f1220cefa2deb5b1ecaf7a94b6c4
l1roii <71549088+l1roii@users.noreply.github.com>
2023-01-08 22:51:40 +0100

    Created method for reporting posts

--- Jobsway2goMvc/Controllers/ReportsController.cs
+++ Jobsway2goMvc/Controllers/ReportsController.cs
@@ -56,5 +56,43 @@ namespace Jobsway2goMvc.Controllers
             return RedirectToAction("Index", "Home");
 
         }
+
+        public async Task<IActionResult> ReportPost(int? id)
+        {
+            if (id == null || _context.Posts == null)
+            {
+                return NotFound();
+            }
+
+            var post = await _context.Posts.FindAsync(id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+
+            return View(post);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ReportPost(Report report, Post post, ReportEnum reportEnum)
+        {
+            var postRef = await _context.Posts.FirstOrDefaultAsync(s => s.Id == post.Id);
+            string model = reportEnum.ToString();
+            ModelState.Remove("JobId");
+            ModelState.Remove("PostId");
+            ModelState.Remove("reportType");
+            ModelState.Remove("GroupId");
+
+            report.Id = 0;
+            report.JobId = null;
+            report.PostId = postRef.Id;
+            report.reportType = reportEnum;
+            report.GroupId = null;
+            _context.Add(report);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index", "Home");
+
+        }
     }
 }
