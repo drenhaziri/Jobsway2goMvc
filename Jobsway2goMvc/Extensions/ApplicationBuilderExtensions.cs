d46d197c7bf0418a0c32516f706b75ebcc6e414a
medinahaliti <medina.haliti11@gmail.com>
2023-01-08 22:13:27 +0100

    added paginated jobs in feed and pagination extensions

---
+++ Jobsway2goMvc/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Jobsway2goMvc.SubscribeTableDependencies;
+
+namespace Jobsway2goMvc.Extensions;
+
+public static class ApplicationBuilderExtensions
+{
+    public static void UseSqlTableDependency<T>(this IApplicationBuilder applicationBuilder, string connectionString)
+            where T : ISubscribeTableDependency
+    {
+        var serviceProvider = applicationBuilder.ApplicationServices;
+        var service = serviceProvider.GetService<T>();
+        service?.SubscribeTableDependency(connectionString);
+    }
+}
---
+++ Jobsway2goMvc/Extensions/PagingExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace Jobsway2goMvc.Extensions;
+
+public static class PagingExtensions
+{
+    public static IEnumerable<T> Page<T>(this IEnumerable<T> items, int pageSize, int page)
+    {
+        return items.Skip((page-1) * pageSize).Take(pageSize);
+    }
+
+    public static IQueryable<T> Page<T>(this IQueryable<T> items, int pageSize, int page)
+    {
+        return items.Skip((page-1) * pageSize).Take(pageSize);
+    }
+}
