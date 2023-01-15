23f1407ca3d6a2f91dfa76d6eece140964046157
Arianit_Ajvazi <arianitajvazi@gmail.com>
2023-01-15 23:01:45 +0100

    Added new private method to validate email format and added rules using regex for firstname and lastname

--- Jobsway2goMvc/Validators/Users/RegistrationValidator.cs
+++ Jobsway2goMvc/Validators/Users/RegistrationValidator.cs
@@ -1,9 +1,10 @@
 ﻿using FluentValidation;
 using Jobsway2goMvc.Areas.Identity.Pages.Account;
+using System.Text.RegularExpressions;
 
 namespace Jobsway2goMvc.Validators.Users
 {
-    public class RegistrationValidator: AbstractValidator<RegisterModel.InputModel>
+    public class RegistrationValidator : AbstractValidator<RegisterModel.InputModel>
     {
         public RegistrationValidator()
         {
@@ -13,10 +14,20 @@ namespace Jobsway2goMvc.Validators.Users
             RuleFor(x => x.LastName)
                 .NotEmpty().WithMessage("This field is required.")
                 .Must(BeValidName).WithMessage("LastName should contain only letters.");
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("This field is required.")
+                .Must(ValidateEmail).WithMessage("The email format is not valid");
         }
         private bool BeValidName(string name)
         {
-            return name.All(Char.IsLetter);
+            var regex = new Regex(@"^[a-zA-Z]+$");
+            return regex.IsMatch(name);
+        }
+        private static bool ValidateEmail(string email)
+        {
+            var regex = @"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?";
+            bool result = Regex.IsMatch(email, regex, RegexOptions.IgnoreCase);
+            return result;
         }
     }
 }
