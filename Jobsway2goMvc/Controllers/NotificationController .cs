a30a2cab37a2226f57bbc7b6ec4558bfa78d2e0f
Muhamet1 <mh51221@ubt-uni.net>
2023-01-24 15:53:25 +0100

    Added notifications dropdown on navbar, mark as read a notification, notification count etc.

---
+++ Jobsway2goMvc/Controllers/NotificationController .cs
@@ -0,0 +1,60 @@
+﻿using Jobsway2goMvc.Data;
+using Jobsway2goMvc.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Jobsway2goMvc.Controllers
+{
+    public class NotificationController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+        public NotificationController (ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var userId =  _userManager.GetUserId(HttpContext.User);
+            var user = await _userManager.FindByIdAsync(userId);
+
+            var notifications = _context.Notifications.Where(n=> n.UserName == user.UserName && n.IsRead == false).ToList();
+            return Json(notifications);
+        }
+        [HttpGet]
+        public async Task<IActionResult> markedAsRead()
+        {
+            var userId = _userManager.GetUserId(HttpContext.User);
+            var user = await _userManager.FindByIdAsync(userId);
+
+            var notifications = _context.Notifications.Where(n => n.UserName == user.UserName && n.IsRead == true).ToList();
+            return Json(notifications);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> countNotifications()
+        {
+            var userId = _userManager.GetUserId(HttpContext.User);
+            var user = await _userManager.FindByIdAsync(userId);
+
+            var notificationCount = _context.Notifications.Where(n => n.UserName == user.UserName && n.IsRead == false).Count();
+            return Json(notificationCount);
+        }
+        [HttpPost]
+        public async Task<IActionResult> markAsRead(int id)
+        {
+            var notification = await _context.Notifications.FirstOrDefaultAsync(n => n.Id == id);
+            if(notification == null)
+            {
+                return NotFound();
+            }
+            notification.IsRead =true;
+            await _context.SaveChangesAsync();
+
+            return Json(new {success = true});
+        }
+    }
+}
--- Jobsway2goMvc/wwwroot/js/site.js
+++ Jobsway2goMvc/wwwroot/js/site.js
@@ -28,3 +28,76 @@ function DisplayPersonalNotification(message, title) {
 
     }, 1300);
 }
+
+//Notification show on navbar
+
+function updateNotifications() {
+    $.ajax({
+        type: "GET",
+        url: "/Notification/Index",
+        dataType: "json",
+        success: function (data) {
+            if (data && data.length > 0) {
+                $("#notificationsContainer").empty();
+                $.each(data, function (i, notification) {
+                    var readButton = "<button class='btn btn-default' onclick='markAsRead(" + notification.id + ")'>Mark as Read</button>";
+                    $("#notificationsContainer").append(readButton + "<a class='dropdown-item'>" + notification.message + "</a>");
+                });
+            } else {
+                $("#notificationsContainer").empty();
+                $("#notificationsContainer").append("<a class='dropdown-item'> No notifications to show</a>");
+            }
+        }
+    });
+}
+
+function markAsRead(id) {
+    $.ajax({
+        type: "POST",
+        url: "/Notification/markAsRead",
+        data: { id: id },
+        success: function (data) {
+            if (data.success) {
+                updateNotifications();
+            }
+        }
+    });
+}
+
+function markedAsRead() {
+    $.ajax({
+        type: "GET",
+        url: "/Notification/markedAsRead",
+        dataType: "json",
+        success: function (data) {
+            if (data && data.length > 0) {
+                $("#notificationsMarkedAsRead").empty();
+                $.each(data, function (i, notification) {
+                    $("#notificationsMarkedAsRead").append("<a class='dropdown-item'>" + notification.message + "</a>");
+                });
+            } else {
+                $("#notificationsMarkedAsRead").empty();
+                $("#notificationsMarkedAsRead").append("<a class='dropdown-item'> No notifications to show</a>");
+            }
+        }
+    });
+}
+
+function getNotificationCount() {
+    $.ajax({
+        type: "GET",
+        url: "/Notification/countNotifications",
+        success: function (data) {
+            $("#notificationCount").text(data);
+        }
+    });
+}
+
+$(document).ready(function () {
+    getNotificationCount();
+    updateNotifications();
+    markedAsRead();
+    setInterval(getNotificationCount, 5000);
+    setInterval(updateNotifications, 5000);
+    setInterval(markedAsRead, 5000);
+});
