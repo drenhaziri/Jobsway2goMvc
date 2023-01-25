f8f59ab497d7798d63d2795e460c36759f7e3de0
l1roii <71549088+l1roii@users.noreply.github.com>
2023-01-25 14:32:22 +0100

    Added listView js file and a list in the index of home

---
+++ Jobsway2goMvc/wwwroot/js/ListView.js
@@ -0,0 +1,22 @@
+﻿const url = "searches.json";
+
+$.getJSON(url, function (data) {
+    var items = [];
+    $.each(data, function (key, val) {
+        if (val.length != 0) {
+            for (var i = 0; i < val.length; i++) {
+                items.push("<li>" + val[i] + "</li>");
+                console.log(val);
+            }
+        }
+        else {
+            console.log("No elements");
+        }
+    });
+
+    $("<ul/>", {
+        "id": "listView",
+        html: items.join("")
+    }).appendTo("body");
+});
+
