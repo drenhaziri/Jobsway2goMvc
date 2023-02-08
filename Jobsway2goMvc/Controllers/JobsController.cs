2e4937ac045e9cb26502af3ef1feb06c0ad96ea7
medinaarexhepii <medinaa.rexhepi1@gmail.com>
2023-02-08 13:18:48 -0800

    fixed

--- Jobsway2goMvc/Controllers/JobsController.cs
+++ Jobsway2goMvc/Controllers/JobsController.cs
@@ -61,7 +61,7 @@ namespace Jobsway2goMvc.Controllers
             var user = GetApplicationUser(userAccessor);
             if (User.IsInRole("Business"))
             {
-                return RedirectToAction("Create");
+                return NotFound("You can not apply");
             }
             ViewBag.HasAlreadyApplied = job.Applicants.Any(a => a.Id == user.Id);
             return View(job);
@@ -72,7 +72,7 @@ namespace Jobsway2goMvc.Controllers
         {
             if (User.IsInRole("Business"))
             {
-                return RedirectToAction("Create");
+                return NotFound("You can not apply");
             }
             var job = await _context.Jobs.FindAsync(id);
             if (job == null)
@@ -91,7 +91,7 @@ namespace Jobsway2goMvc.Controllers
         {
             if (User.IsInRole("Business"))
             {
-                return RedirectToAction("Create");
+                return NotFound("You can not apply");
             }
             var job = await _context.Jobs
                 .Include(j => j.Applicants)
@@ -106,63 +106,6 @@ namespace Jobsway2goMvc.Controllers
             await _context.SaveChangesAsync();
             return RedirectToAction("ApplyJob", new { id = id });
         }
-
-
-
-
-        //[HttpGet]
-        //public async Task<IActionResult> ApplyJob(int? id)
-        //{
-        //    if (id == null || _context.Jobs == null)
-        //    {
-        //        return NotFound();
-        //    }
-        //    var job = await _context.Jobs
-        //         .Include(j => j.Category)
-        //         .Include(j => j.Applicants)
-        //         .FirstOrDefaultAsync(m => m.Id == id);
-        //    if (job == null)
-        //    {
-        //        return NotFound();
-        //    }
-        //    var userAccessor = _httpContextAccessor.HttpContext.User;
-        //    var user = GetApplicationUser(userAccessor);
-        //    ViewBag.HasAlreadyApplied = job.Applicants.Any(a => a.Id == user.Id);
-        //    return View(job);
-        //}
-
-
-        //[HttpPost]
-        //public async Task<IActionResult> ApplyJob(int id)
-        //{
-        //    var job = await _context.Jobs.FindAsync(id);
-        //    if (job == null)
-        //    {
-        //        return NotFound();
-        //    }
-        //    var userAccessor = _httpContextAccessor.HttpContext.User;
-        //    var user = GetApplicationUser(userAccessor);
-        //    job.Applicants.Add(user);
-        //    await _context.SaveChangesAsync();
-        //    return RedirectToAction("ApplyJob", new { id = id });
-        //}
-
-        //[HttpPost]
-        //public async Task<IActionResult> UnApplyJob(int id)
-        //{
-        //    var job = await _context.Jobs
-        //        .Include(j => j.Applicants)
-        //        .FirstOrDefaultAsync(m => m.Id == id);
-        //    if (job == null)
-        //    {
-        //        return NotFound();
-        //    }
-        //    var userAccessor = _httpContextAccessor.HttpContext.User;
-        //    var user = GetApplicationUser(userAccessor);
-        //    job.Applicants.Remove(user);
-        //    await _context.SaveChangesAsync();
-        //    return RedirectToAction("ApplyJob", new { id = id });
-        //}
 
         public async Task<IActionResult> Details(int? id)
         {
