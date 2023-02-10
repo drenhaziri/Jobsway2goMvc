6024ef710c794fd318cf6ba9302137c7eff18f8f
Muhamet1 <mh51221@ubt-uni.net>
2023-02-10 19:58:55 +0100

    Add rules at fluent validator for location, site, position. if selected enum is none to throw errorr message

--- Jobsway2goMvc/Validators/Jobs/JobValidator.cs
+++ Jobsway2goMvc/Validators/Jobs/JobValidator.cs
@@ -1,4 +1,5 @@
 ﻿using FluentValidation;
+using Jobsway2goMvc.Enums;
 using Jobsway2goMvc.Models;
 
 namespace Jobsway2goMvc.Validators.Jobs
@@ -8,8 +9,24 @@ namespace Jobsway2goMvc.Validators.Jobs
         public JobValidator()
         {
             RuleFor(e => e.CompanyName).NotEmpty().WithMessage("Please specify a company name");
-            RuleFor(e => e.Location).NotEmpty().WithMessage("Please specify a location");
-            RuleFor(e => e.Schedule).NotEmpty().WithMessage("Please specify a Schedule");
+            RuleFor(e => e.Location).Must((e, location) => {
+                if (location == JobLocation.None)
+                    return false;
+
+                return true;
+            }).WithMessage("Please specify a location");
+            RuleFor(e => e.Schedule).Must((e, position) => {
+                if (position == JobPosition.None)
+                    return false;
+
+                return true;
+            }).WithMessage("Please specify a schedule");
+            RuleFor(e => e.Site).Must((e, site) => {
+                if (site == JobSite.None)
+                    return false;
+
+                return true;
+            }).WithMessage("Please specify a site");
             RuleFor(e => e.Description).NotEmpty().WithMessage("Please specify a description");
             RuleFor(e => e.OpenSpots).NotEmpty().WithMessage("Please specify an OpenSpots");
             RuleFor(e => e.Requirements).NotEmpty().WithMessage("Please specify Requirements");
